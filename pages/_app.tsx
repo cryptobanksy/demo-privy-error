3263cb58de65ceebf3d6be15f5956e058d5cc50f
CryptoBanksy <blockwealth@qq.com>
2024-04-22 18:55:34 +0800

    feat: demo chiliz testnet error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,45 @@ import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import Head from 'next/head';
 import {PrivyProvider} from '@privy-io/react-auth';
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
+// import { defineChain } from "viem";
+
+import {
+  bscTestnet,
+  // chiliz,
+  spicy,
+} from "viem/chains";
+
+// const chilizTest = defineChain({
+//   id: 88882,
+//   name: "Chiliz Spicy Testnet",
+//   network: "chiliz",
+//   nativeCurrency: {
+//     decimals: 18,
+//     name: "CHZ",
+//     symbol: "CHZ",
+//   },
+//   rpcUrls: {
+//     public: { http: ["https://spicy-rpc.chiliz.com/"] },
+//     default: { http: ["https://spicy-rpc.chiliz.com/"] },
+//   },
+//   blockExplorers: {
+//     etherscan: { name: "ChilizScan", url: "https://testnet.chiliscan.com/" },
+//     default: { name: "ChilizScan", url: "https://testnet.chiliscan.com/" },
+//   },
+//   contracts: {
+//     multicall3: {
+//       address: "0xca11bde05977b3631167028862be2a173976ca11",
+//       blockCreated: 8080847,
+//     },
+//   },
+// });
 
 function MyApp({Component, pageProps}: AppProps) {
   const router = useRouter();
 
   return (
-    <>
+    <div>
       <Head>
         <link rel="preload" href="/fonts/AdelleSans-Regular.woff" as="font" crossOrigin="" />
         <link rel="preload" href="/fonts/AdelleSans-Regular.woff2" as="font" crossOrigin="" />
@@ -24,12 +56,19 @@ function MyApp({Component, pageProps}: AppProps) {
         <meta name="description" content="Privy Auth Starter" />
       </Head>
       <PrivyProvider
-        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
-        onSuccess={() => router.push('/dashboard')}
+        appId="clnju0ny30cfhl60frmw59ker"
+        config={{
+          supportedChains: [spicy, bscTestnet],
+          customAuth: {
+            isLoading: false,
+            // getCustomAccessToken: async () => "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmb290YmFsbGNyYWZ0LmlvIiwiZXhwIjoxNzQwNjcyMDAwLCJzdWIiOiI4NjAwMDAwMDAwMDAwMDAwNTciLCJhdWQiOiJmb290YmFsbGNyYWZ0LmlvIiwibmJmIjoxNzA5MTg2NjgyLCJpYXQiOjE3MDkxODY2ODV9.KLO_ZKkqumPdWgzqBd4iJdjxTYjTzJE73ph-84EiNudvC35zp2wlmlUSDKbdVEm9nY_y78rRUXbsilSJOc0YCJwrifxYWqmbJr90FguAKL9HZFBeAa26XNpfDN9LTKTu0xRLQNYJL5Dp2pdWWFjz7Ju2dAdcH2uwZ_Pko84K66B9dp5qt5ljUmySLVFKS0NRRSI_63aN4Qy8tP5YBhp3yfYY7Ky5oLXLMXjjWeaEnXde80VZtmaxdicYJqVNjeQ9o-A5M7pAeaOtdcGSJlRS7U7oLfYOkGIIugmJAcC5ROhvXaodNblCEb2lFmjgIWhpDtZ7XUgHn-TcztaebVX-Qg",
+            getCustomAccessToken: async () => "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDU2MjQwNDYwZmU0MjEyYjA3MmFmNDUiLCJuYmYiOjE3MTE4NTU0NDksImV4cCI6MTcxMjExNDk0OSwiaWF0IjoxNzExODU1NDQ5fQ.Hn4I9JsfM4e6tOXuaIQYyC5QbjcT9dld4GRtLJtcMmgGGRwlBRahBHp0uMDxrT9fV4ZuGFRnp4yICMm8UgrF2cGOd9Oa3Eq0J-N5iMVbZjwBLLm8q4B0go-gUMmhfKNtRScUoWv52XrOJpbe169F-arPh65CNh6xuL1NkVl5WcnMYMxDiccK93Xjapk1ufEx5z05-lLEUoJ3zp21U_--1iGbPDkyTAdzh_r-FjH-IYhsd_zkquEDUhwds8HLh_kEfe7VZ7TD1J3De0cI7fSVnQSeNKY3yfJpLIc7ihW1F9Aii7uh_5VoDBvlrOBDdzxE6rohupKK4LRpHtmL1bCQnQ",
+          },
+        }}
       >
         <Component {...pageProps} />
       </PrivyProvider>
-    </>
+    </div>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,113 @@
-import Portal from '../components/graphics/portal';
-import {usePrivy} from '@privy-io/react-auth';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { ConnectedWallet, usePrivy, useWallets } from '@privy-io/react-auth';
 import Head from 'next/head';
+import { ethers } from 'ethers';
 
-export default function LoginPage() {
-  const {login} = usePrivy();
+export default function DashboardPage() {
+  const {
+    ready,
+    authenticated,
+    user,
+  } = usePrivy();
+
+  const { wallets } = useWallets();
+  const embeddedWallet: ConnectedWallet | undefined = useMemo(() => {
+    return wallets.find((w) => w.walletClientType === "privy");
+  }, [wallets]);
+
+  const getEthersSigner = 
+    async () => {
+      const provider = await embeddedWallet?.getEthersProvider();
+      return await provider?.getSigner();
+    };
+
+  const [chainId, setChainId] = useState("88882")
+  const [balance, setBalance] = useState<any>(0)
+  const fetchBalance = async () => {
+    const signer = await getEthersSigner();
+    if (!signer) return;
+    const d = await signer.getBalance();
+    const balance = ethers.formatEther(d?.toBigInt());
+    setBalance(balance);
+  };
+
+  useEffect(() => {
+    fetchBalance().catch(() => {})
+  }, [embeddedWallet])
+
+  const [result, setResult] = useState<any>()
+
+  const handleSend = async () => {
+    setResult("")
+    // chiliz spicy network 88882
+    await embeddedWallet?.switchChain(Number(chainId));
+    console.log(embeddedWallet?.chainId);
+
+    const signer = await getEthersSigner();
+    if (!signer) {
+      return;
+    }
+
+    console.log(signer);
+
+    try {
+      const tx = await signer.sendTransaction({
+        to: "0x62c717299AbfF852385173ffEf45B9F48D86c5b4",
+        value: ethers.parseEther("0.1"),
+      });
+
+      const result = await tx.wait();
+      setResult(result)
+
+    } catch (e) {
+      console.log(e);
+      setResult(e)
+    }
+    
+  }
 
   return (
     <>
       <Head>
-        <title>Login · Privy</title>
+        <title>Privy Auth Demo</title>
       </Head>
 
-      <main className="flex min-h-screen min-w-full">
-        <div className="flex bg-privy-light-blue flex-1 p-6 justify-center items-center">
-          <div>
+      <main className="flex flex-col min-h-screen px-4 sm:px-20 py-6 sm:py-10 bg-privy-light-blue">
+        {ready && authenticated ? (
+          <>
+            <p className="mt-6 font-bold uppercase text-sm text-gray-600">User object</p>
+            <textarea
+              value={JSON.stringify(user, null, 2)}
+              className="max-w-4xl bg-slate-700 text-slate-50 font-mono p-4 text-xs sm:text-sm rounded-md mt-2"
+              rows={8}
+              disabled
+            />
+            <p className="mt-6 font-bold uppercase text-sm text-gray-600">Embedded Wallet object</p>
+            <textarea
+              value={JSON.stringify(embeddedWallet, null, 2)}
+              className="max-w-4xl bg-slate-700 text-slate-50 font-mono p-4 text-xs sm:text-sm rounded-md mt-2"
+              rows={8}
+              disabled
+            />
+
             <div>
-              <Portal style={{maxWidth: '100%', height: 'auto'}} />
+              <select className="mt-5 form-select px-4 py-3 w-[200px]" value={chainId} onChange={(e) => {
+                console.log('select', e.target.value)
+                setChainId(e.target.value)
+                embeddedWallet?.switchChain(Number(e.target.value))
+              }}>
+                <option value="97">BNB Chain</option>
+                <option value="88882">Chiliz Chain</option>
+              </select>
+              <div>Balance: { balance }</div>
+              <button className='bg-red-400 rounded-lg py-2 px-5 mt-6' onClick={handleSend}>Send</button>
+
+              <div>
+                {JSON.stringify(result, null, 2)}
+              </div>
             </div>
-            <div className="mt-6 flex justify-center text-center">
-              <button
-                className="bg-violet-600 hover:bg-violet-700 py-3 px-6 text-white rounded-lg"
-                onClick={login}
-              >
-                Log in
-              </button>
-            </div>
-          </div>
-        </div>
+          </>
+        ) : null}
       </main>
     </>
   );
